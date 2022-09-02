7311b26e4739464727d142bd751a529959520759
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-09-02 10:04:12 -0300

    Requisito 08

--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -14,23 +14,22 @@ class MusicCard extends React.Component {
     };
 
     this.favoring = this.favoring.bind(this);
+    this.handleClickCheck = this.handleClickCheck.bind(this);
+  }
+
+  handleClickCheck(/* event */) {
+    // const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      messageLoading: true,
+    }, () => {
+      const time = 1000;
+      setTimeout(() => this.setState({ messageLoading: false }), time);
+    });
   }
 
   async favoring(album) {
-    this.setState({
-      messageLoading: true,
-      // songsSaved: returnAPI,
-    }, () => this.setState({ messageLoading: false }));
     const returnAPI = await addSong(album);
-    // const returnAPI = await addSong(album);
-    // this.setState({
-    //   messageLoading: true,
-    // }, () => {
-    //   const time = 200;
-    //   if (returnAPI === 'OK') {
-    //     setTimeout(() => this.setState({ messageLoading: false }), time);
-    //   }
-    // });
+    if (returnAPI === 'OK') this.setState({ songsSaved: returnAPI });
   }
 
   render() {
@@ -39,6 +38,7 @@ class MusicCard extends React.Component {
     return (
       <div>
         {messageLoading && <Loading />}
+        {songsSaved === 'OK' && 'Graças a deus!'}
         <section>
           {albumFull.length > 0 && (
             <ul className="listaMusicasPai">
@@ -67,12 +67,14 @@ class MusicCard extends React.Component {
                         .
                       </audio>
                       <label htmlFor="fieldFavorite">
-                        Favorita
+                        {/* Favorita */}
                         <input
                           type="checkbox"
                           id="fieldFavorite"
                           data-testid={ `checkbox-music-${e.trackId}` }
                           onClick={ () => this.favoring(e) }
+                          onChange={ this.handleClickCheck }
+                          className="favoriteSong"
                         />
                       </label>
                     </div>
@@ -92,52 +94,3 @@ MusicCard.propTypes = {
 };
 
 export default MusicCard;
-
-// {messageLoading
-//   ? (
-//     <Carregando />
-//   ) : (
-//     <section>
-//       {albumFull.length > 0 && (
-//         <ul className="listaMusicasPai">
-//           {albumFull.map((e, i) => (
-//             <li key={ `${e.artistName}-${i}` }>
-//               {i === 0 && (
-//                 <div className="album">
-//                   <img src={ e.artworkUrl100 } alt={ e.artistName } />
-//                   <h2 data-testid="album-name">{e.collectionName}</h2>
-//                   <p data-testid="artist-name">{e.artistName}</p>
-//                 </div>
-//               )}
-//               {i > 0 && (
-//                 <div>
-//                   <h3>{e.trackName}</h3>
-//                   <audio
-//                     data-testid="audio-component"
-//                     src={ e.previewUrl }
-//                     controls
-//                   >
-//                     <track kind="captions" />
-//                     O seu navegador não suporta o elemento
-//                     {' '}
-//                     {' '}
-//                     <code>audio</code>
-//                     .
-//                   </audio>
-//                   <label htmlFor="fieldFavorite">
-//                     Favorita
-//                     <input
-//                       type="checkbox"
-//                       id="fieldFavorite"
-//                       data-testid={ `checkbox-music-${e.trackId}` }
-//                       onClick={ () => this.favoring(e) }
-//                     />
-//                   </label>
-//                 </div>
-//               )}
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//     </section>
-//   )}
