8d8d44f69482abfa48c3b604c508844dca429a74
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-12-20 20:00:51 -0300

    test/refactor: Melhorando a cobertura de testes da página de buscas da aplicação

--- src/tests/02-Search.test.js
+++ src/tests/02-Search.test.js
@@ -1,67 +1,66 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import renderWithRouter from './helpers/renderWithRouter';
-import Search from '../pages/Search';
+import { screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as searchAlbumsAPI from '../services/searchAlbumsAPI';
+import renderPath from './helpers/renderPath';
+
 
 describe('Testes direcionados ao comportamento da página Search...', () => {
   it('Existe um botão para navegar até a página de Login da aplicação: ', () => {
-    renderWithRouter(<Search />);
+    renderPath('/search');
 
     const home = screen.getByRole('link', { name: /home/i });
     expect(home).toBeInTheDocument();
   });
 
   it('Existe um botão para navegar até a página de buscas de álbuns/artistas: ', () => {
-    renderWithRouter(<Search />);
+    renderPath('/search');
 
     const search = screen.getByRole('link', { name: /search/i });
     expect(search).toBeInTheDocument();
   });
 
   it('Existe um botão para navegar até a página de músicas favoritadas: ', () => {
-    renderWithRouter(<Search />);
+    renderPath('/search');
 
     const favorites = screen.getByRole('link', { name: /favorites/i });
     expect(favorites).toBeInTheDocument();
   });
 
   it('Existe um botão para navegar até a página de Perfil do usuário: ', () => {
-    renderWithRouter(<Search />);
+    renderPath('/search');
 
     const profile = screen.getByRole('link', { name: /profile/i });
     expect(profile).toBeInTheDocument();
   });
 
   it('Existe um campo para pesquisar por álbuns ou artistas: ', () => {
-    renderWithRouter(<Search />);
+    renderPath('/search');
 
     const campoBusca = screen.getByRole('textbox');
     expect(campoBusca).toBeInTheDocument();
   });
 
   it('Existe um botão para iniciar a busca de álbuns ou artistas: ', () => {
-    renderWithRouter(<Search />);
+    renderPath('/search');
 
     const botaoPesquisa = screen.getByRole('button', { name: /pesquisar/i });
     expect(botaoPesquisa).toBeInTheDocument();
   });
 
-  // it('É possível pesquisar álbum ou artista passando um nome válido: ', async () => {
-  //   jest.spyOn(global, 'fetch').mockResolvedValue({
-  //     json: jest.fn().mockResolvedValue(results),
-  //   });
-  //   renderWithRouter(<Search />);
+  it('É possível pesquisar álbum ou artista passando um nome válido: ', async () => {
+    const mockAlbums = jest.spyOn(searchAlbumsAPI, 'default');
 
-  //   const campoBusca = screen.getByRole('textbox');
-  //   const TERM = 'Raimundos';
-  //   userEvent.type(campoBusca, TERM);
-  //   expect(campoBusca).toBeInTheDocument();
+    renderPath('/search');
 
-  //   const botaoPesquisa = screen.getByRole('button', { name: /pesquisar/i });
-  //   userEvent.click(botaoPesquisa);
-  //   expect(botaoPesquisa).toBeInTheDocument();
+    const campoBusca = screen.getByRole('textbox');
+    expect(campoBusca).toBeInTheDocument();
+    userEvent.type(campoBusca, 'Raimundos');
+    expect(campoBusca.value).toBe('Raimundos');
 
-  //   await searchAlbumsAPI(TERM);
-  //   expect(searchAlbumsAPI).toHaveBeenCalled();
-  // });
+    const botaoPesquisa = screen.getByRole('button', { name: /pesquisar/i });
+    expect(botaoPesquisa).toBeInTheDocument()
+    userEvent.click(botaoPesquisa);
+
+    await waitFor(() => expect(mockAlbums).toBeCalled(), { timeout: 3000 });
+  });
 });
